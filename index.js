7d175ddf7115a94087fbf50687e6737e5b2cee97
*abrahamnyuiadzi* <abrahamnyuiadzi>
2025-07-28 17:38:50 +0000

    chnage

---
+++ index.js
@@ -0,0 +1,24 @@
+ //function to add the task
+    function addTask() {
+      //get the id taskInput
+      const input = document.getElementById("taskInput");
+      const taskText = input.value.trim();
+
+      if (taskText === "") return;
+
+      const li = document.createElement("li");
+      li.innerHTML = `
+        <span>${taskText}</span>
+        <button class="delete-btn" onclick="deleteTask(this)">Supprimer</button>
+      `;
+
+      document.getElementById("taskList").appendChild(li);
+      input.value = "";
+    }
+    //delete function to delete the tasks
+
+    function deleteTask(button) {
+      const li = button.parentElement;
+      li.remove();
+    }
+ 
